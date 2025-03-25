9ec368049552bf4bfc18ae1a688b99c5ffb78ba1
ian <ianechu1810@gmail.com>
2025-03-25 12:38:47 +0100

    Signed-off-by: ian <ianechu1810@gmail.com>

--- bicis/src/bicis/Bici.java
+++ bicis/src/bicis/Bici.java
@@ -17,6 +17,7 @@ import javax.swing.table.TableModel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 
 public class Bici {
 
@@ -24,6 +25,8 @@ public class Bici {
 	private JTextField textFieldnombre;
 	private JTextField textFieldEdad;
 	private JTextField textFieldBanco;
+	private JTextField textFieldIdUsuario;
+	private JTextField textFieldIdBici;
 
 	/**
 	 * Launch the application.
@@ -47,6 +50,7 @@ public class Bici {
 	public Bici() {
 		initialize();
 	}
+
 	class ConnectionSingleton {
 		private static Connection con;
 
@@ -60,6 +64,7 @@ public class Bici {
 			return con;
 		}
 	}
+
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -68,40 +73,50 @@ public class Bici {
 		frame.setBounds(100, 100, 692, 692);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
+
 		textFieldnombre = new JTextField();
 		textFieldnombre.setBounds(51, 468, 114, 19);
 		frame.getContentPane().add(textFieldnombre);
 		textFieldnombre.setColumns(10);
-		
+
 		textFieldEdad = new JTextField();
 		textFieldEdad.setBounds(51, 523, 114, 19);
 		frame.getContentPane().add(textFieldEdad);
 		textFieldEdad.setColumns(10);
-		
+
 		textFieldBanco = new JTextField();
 		textFieldBanco.setBounds(51, 578, 114, 19);
 		frame.getContentPane().add(textFieldBanco);
 		textFieldBanco.setColumns(10);
 		
+		textFieldIdUsuario = new JTextField();
+		textFieldIdUsuario.setBounds(257, 523, 114, 19);
+		frame.getContentPane().add(textFieldIdUsuario);
+		textFieldIdUsuario.setColumns(10);
+		
+		textFieldIdBici = new JTextField();
+		textFieldIdBici.setBounds(257, 578, 114, 19);
+		frame.getContentPane().add(textFieldIdBici);
+		textFieldIdBici.setColumns(10);
+
 		JLabel lblNombre = new JLabel("Nombre");
 		lblNombre.setBounds(61, 443, 70, 15);
 		frame.getContentPane().add(lblNombre);
-		
+
 		JLabel lblEdad = new JLabel("Edad");
 		lblEdad.setBounds(61, 499, 70, 15);
 		frame.getContentPane().add(lblEdad);
-		
+
 		JLabel lblCuentaBancaria = new JLabel("Cuenta Bancaria");
 		lblCuentaBancaria.setBounds(61, 551, 141, 15);
 		frame.getContentPane().add(lblCuentaBancaria);
-		
+
 		DefaultTableModel model1 = new DefaultTableModel();
 		model1.addColumn("ID");
 		model1.addColumn("Nombre");
 		model1.addColumn("Edad");
 		model1.addColumn("Cuenta bancaria");
-		
+
 		try {
 			Connection con = ConnectionSingleton.getConnection();
 			Statement stmt = con.createStatement();
@@ -117,11 +132,11 @@ public class Bici {
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
-		
+
 		DefaultTableModel model2 = new DefaultTableModel();
 		model2.addColumn("Numero de bicicleta");
 		model2.addColumn("ID usuario");
-		
+
 		try {
 			Connection con = ConnectionSingleton.getConnection();
 			Statement stmt = con.createStatement();
@@ -135,7 +150,7 @@ public class Bici {
 		} catch (SQLException ex) {
 			ex.printStackTrace();
 		}
-		
+
 		JTable table1 = new JTable(model1);
 		table1.setAutoResizeMode(JTable.AUTO_RESIZE_ALL_COLUMNS);
 		table1.setBounds(71, 56, 146, 74);
@@ -144,7 +159,7 @@ public class Bici {
 		scrollPane.setBounds(41, 12, 287, 227);
 
 		frame.getContentPane().add(scrollPane);
-		
+
 		table1.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
@@ -152,6 +167,7 @@ public class Bici {
 				int index = table1.getSelectedRow();
 				TableModel model = table1.getModel();
 				// ID NAME AGE CITY
+				textFieldIdUsuario.setText(model1.getValueAt(index, 0).toString());
 				textFieldnombre.setText(model1.getValueAt(index, 1).toString());
 				textFieldEdad.setText(model1.getValueAt(index, 2).toString());
 				textFieldBanco.setText(model1.getValueAt(index, 3).toString());
@@ -167,23 +183,36 @@ public class Bici {
 
 		frame.getContentPane().add(scrollPane2);
 		
+		table2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+
+				int index = table2.getSelectedRow();
+				TableModel model2 = table2.getModel();
+				// ID NAME AGE CITY
+				
+				textFieldIdBici.setText(model2.getValueAt(index, 0).toString());
+			}
+		});
+
 		JButton btnAadirUsuario = new JButton("Añadir usuario");
 		btnAadirUsuario.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
+
 				try {
 					Connection con = ConnectionSingleton.getConnection();
-					PreparedStatement pstmt_anadir_usuario = con.prepareStatement("INSERT INTO usuario (nombre, edad, cuenta_bancaria) VALUES(?, ?, ?)");
+					PreparedStatement pstmt_anadir_usuario = con
+							.prepareStatement("INSERT INTO usuario (nombre, edad, cuenta_bancaria) VALUES(?, ?, ?)");
 					pstmt_anadir_usuario.setString(1, textFieldnombre.getText());
-					pstmt_anadir_usuario.setInt(2,Integer.parseInt( textFieldEdad.getText()));
+					pstmt_anadir_usuario.setInt(2, Integer.parseInt(textFieldEdad.getText()));
 					pstmt_anadir_usuario.setInt(3, Integer.parseInt(textFieldBanco.getText()));
 					int rowsUpdated = pstmt_anadir_usuario.executeUpdate();
 					pstmt_anadir_usuario.close();
 					Statement pstmt_mostrar = con.createStatement();
 					ResultSet rs_mostrar = pstmt_mostrar.executeQuery("SELECT * FROM usuario");
 					model1.setRowCount(0);
-					while(rs_mostrar.next()) {
+					while (rs_mostrar.next()) {
 						Object[] row = new Object[4];
 						row[0] = rs_mostrar.getString("idusuario");
 						row[1] = rs_mostrar.getString("nombre");
@@ -196,27 +225,30 @@ public class Bici {
 					// TODO Auto-generated catch block
 					e1.printStackTrace();
 				}
-				
-				
+
 			}
 		});
 		btnAadirUsuario.setBounds(51, 256, 185, 25);
 		frame.getContentPane().add(btnAadirUsuario);
-		
+
 		JButton btnAadirBicicleta = new JButton("Añadir bicicleta");
 		btnAadirBicicleta.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				try {
 					Connection con = ConnectionSingleton.getConnection();
-					PreparedStatement pstmt_anadir_bici = con.prepareStatement("INSERT INTO bicicleta (idbicicleta) values (null)");
+					PreparedStatement pstmt_anadir_bici = con
+							.prepareStatement("INSERT INTO bicicleta (idusuario) values (0)");
 					int rowsUpdated = pstmt_anadir_bici.executeUpdate();
-					pstmt_anadir_bici.close();
 					Statement stmt = con.createStatement();
 					ResultSet rs = stmt.executeQuery("SELECT * FROM bicicleta");
+					rowsUpdated = pstmt_anadir_bici.executeUpdate();
+					pstmt_anadir_bici.close();
+					model2.setRowCount(0);
 					while (rs.next()) {
-						Object[] row = new Object[1];
-						row[0] = rs.getString("idbicicleta");
+						Object[] row = new Object[2];
+						row[0] = rs.getInt("idbicicleta");
+						row[1] = rs.getInt("idusuario");
 						model2.addRow(row);
 					}
 				} catch (SQLException ex) {
@@ -226,18 +258,83 @@ public class Bici {
 		});
 		btnAadirBicicleta.setBounds(50, 303, 186, 25);
 		frame.getContentPane().add(btnAadirBicicleta);
-		
+
 		JButton btnAlquilarBicicleta = new JButton("Alquilar bicicleta");
+		btnAlquilarBicicleta.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					Connection con = ConnectionSingleton.getConnection();
+					PreparedStatement pstmt_comprobar_alquilar_bici = con
+							.prepareStatement("SELECT idusuario FROM bicicleta WHERE idbicicleta = ?");
+					pstmt_comprobar_alquilar_bici.setInt(1, Integer.parseInt(textFieldIdBici.getText()));
+		            ResultSet rsCheck = pstmt_comprobar_alquilar_bici.executeQuery();
+
+		            if (rsCheck.next()) {
+		                int currentIdUsuario = rsCheck.getInt("idusuario");
+
+		                if (currentIdUsuario != 0) {
+		                    JOptionPane.showMessageDialog(null, "Esta bicicleta ya está alquilada y no se puede cambiar el usuario.");
+		                    
+		                }else {
+					PreparedStatement pstmt_alquilar_bici = con
+							.prepareStatement("UPDATE bicicleta SET idusuario = ? WHERE idbicicleta = ?;");
+					pstmt_alquilar_bici.setInt(1, Integer.parseInt(textFieldIdUsuario.getText()));
+					pstmt_alquilar_bici.setInt(2, Integer.parseInt(textFieldIdBici.getText()));				
+					int rowsUpdated = pstmt_alquilar_bici.executeUpdate();
+					pstmt_alquilar_bici.close();
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery("SELECT * FROM bicicleta");
+					model2.setRowCount(0);
+					while (rs.next()) {
+						Object[] row = new Object[2];
+						row[0] = rs.getInt("idbicicleta");
+						row[1] = rs.getInt("idusuario");
+						model2.addRow(row);
+					}
+		                }}} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 		btnAlquilarBicicleta.setBounds(51, 357, 185, 25);
 		frame.getContentPane().add(btnAlquilarBicicleta);
-		
+
 		JButton btnDevolverBicicleta = new JButton("Devolver bicicleta");
+		btnDevolverBicicleta.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				try {
+					Connection con = ConnectionSingleton.getConnection();
+					PreparedStatement pstmt_devolver_bici = con
+							.prepareStatement("UPDATE bicicleta SET idusuario = 0 WHERE idbicicleta = ?;");
+					pstmt_devolver_bici.setInt(1, Integer.parseInt(textFieldIdBici.getText()));
+					int rowsUpdated = pstmt_devolver_bici.executeUpdate();
+					pstmt_devolver_bici.close();
+					Statement stmt = con.createStatement();
+					ResultSet rs = stmt.executeQuery("SELECT * FROM bicicleta");
+					model2.setRowCount(0);
+					while (rs.next()) {
+						Object[] row = new Object[2];
+						row[0] = rs.getInt("idbicicleta");
+						row[1] = rs.getInt("idusuario");
+						model2.addRow(row);
+					}
+				} catch (SQLException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
 		btnDevolverBicicleta.setBounds(51, 406, 185, 25);
 		frame.getContentPane().add(btnDevolverBicicleta);
 		
+		JLabel lblIdUsuario = new JLabel("Id usuario");
+		lblIdUsuario.setBounds(258, 499, 98, 15);
+		frame.getContentPane().add(lblIdUsuario);
 		
-		
-		
-	}
-	}
+		JLabel lblIdBicicleta = new JLabel("Id bicicleta");
+		lblIdBicicleta.setBounds(257, 554, 99, 15);
+		frame.getContentPane().add(lblIdBicicleta);
 
+	}
+}
